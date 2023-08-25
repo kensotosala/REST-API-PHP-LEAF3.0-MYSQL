6ebb98a63b243719000240e57e064bf8992941be
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-24 21:55:39 -0600

    Conexión a BD

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -4,6 +4,10 @@ app()->get("/", function () {
     response()->json(["message" => "Congrats!! You're on Leaf API"]);
 });
 
+app()->get("/web", function () {
+    response()->json(["message" => "Sí, funciona!"]);
+});
+
 app()->get("/app", function () {
     // app() returns $app
     response()->json(app()->routes());
